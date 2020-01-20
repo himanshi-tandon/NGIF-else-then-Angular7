333fc9860ea3eb7db9c5661eb7f1b42f7af59f78
Tandon <ht250132@ncr.com>
2020-01-21 00:16:43 +0530

    IFELSETHENEXAMPLE commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  ISvalid:boolean=true;
+  Change_radio(valid){
+   this.ISvalid=valid;
+  }
+  constructor(){
+    console.log("Hi This component");
+  }
   title = 'Angular7app';
+
+//   @HostListener('click',['$event'])
+// onhostclick(event:Event){
+// alert("Hello learn Hostlistner");s
+// }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {DragDropModule} from '@angular/cdk/drag-drop'
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor(){
+    console.log("Work at the run time");
+  }
+}
